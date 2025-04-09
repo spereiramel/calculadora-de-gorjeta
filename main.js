d4b8a58ac7497350fb5c74642dbfa0b186c71e0c
meelyssa2411 <meelyssa2411@gmail.com>
2025-04-09 23:14:48 +0000

    Criação do JavaScript, da função para receber o valor da conta e da função para receber o número de pessoas

---
+++ main.js
@@ -0,0 +1,11 @@
+let bill = 0
+let tipPercentage = 0
+let numberOfPeople = 0
+
+function receiveBillValue() {
+    bill = document.querySelector("#bill").valueAsNumber
+}
+
+function receiveNumberOfPeopleValue() {
+    numberOfPeople = document.querySelector("#people").valueAsNumber
+}
